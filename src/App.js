a1eb31fd9748127e71dc2e449f5cbdd8fbffa3c5
mertatc <mertatc1903@hotmail.com>
2023-01-29 23:47:53 +0300

    api

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
 import './App.css';
+import axios from 'axios';
+import { useEffect } from 'react';
+
 
 function App() {
+
+  const key="1e06bd6c8dd72e862fdc5a0c15f48cea";
+
+  useEffect(() => {
+    async function getApi() {
+      try {
+        const response = await axios.get(`https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=${key}&units=metric`);
+        console.log(response);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    getApi();
+  },[])
   return (
     <div className="App">
       HELLO
