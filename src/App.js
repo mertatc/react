ac71b31769cabd646f9668f6642891a9a32f97a9
mertatc <mertatc1903@hotmail.com>
2023-01-30 12:27:32 +0300

    Countinue

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
 import './App.css';
-import axios from 'axios';
-import { useEffect } from 'react';
+import React, {useState} from 'react';
 
 
 function App() {
 
   const key="1e06bd6c8dd72e862fdc5a0c15f48cea";
-
-  useEffect(() => {
-    async function getApi() {
-      try {
-        const response = await axios.get(`https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=${key}&units=metric`);
-        console.log(response);
-      } catch (error) {
-        console.error(error);
-      }
-    }
-    getApi();
-  },[])
+  const [weatherData, setWeatherData] = useState([{}]);
+ 
   return (
-    <div className="App">
-      HELLO
+    <div className="container">
+      <div>
+        <input className="input" placeholder="Enter Ctiy..."/>
+      </div>
     </div>
   );
 }
